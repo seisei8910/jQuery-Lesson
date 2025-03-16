7d64dd924c1d1e058f391fc305c5f6797e705165
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-03-16 03:58:15 +0000

    5章 jQueryでHTMLのCSS要素を操作しよう 確認問題

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 $(function(){
-  $('.box1').hide(2000);
+  $('.box1').slideDown(1000, function(){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'height': '100px'
+    }).slideUp(1000);
+  });
 });
