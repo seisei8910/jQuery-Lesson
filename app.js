0cfd44eb9a37cb824ca4e3dff1bf68667289bbab
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-03-16 04:11:35 +0000

    『マウスオーバー』イベントを設定

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 $(function(){
-  $('.box1').slideDown(1000, function(){
-    $('.box1').css({
-      'background-color': '#0000FF',
-      'height': '100px'
-    }).slideUp(1000);
+  $('.box1').mouseover(function(){
+    $('.box1').css({'background-color': '#0000FF'});
   });
 });
